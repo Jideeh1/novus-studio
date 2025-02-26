f38bc5250215a54c6983b5d3e295c85fc88a0981
Jideeh1 <keonagjd@gmail.com>
2025-02-26 10:43:58 +0800

    Add files via upload

--- Team Novus/script.js
+++ Team Novus/script.js
@@ -1,3 +1,9 @@
+// Comments just in case someone snoop on my code through inspect element
+console.log("If you're here to snoop on my messy code, reconsider before your head starts hurting.\nSeriously tho, stop. If you just want to take some snippets or look at the whole code, it's available in my github.")
+console.log("Github Link: https://github.com/Jideeh1/novus-studio")
+console.log("It's also hosted on netlify, but I forgot the link so eh.")
+console.log("Just don't complain when you see how messy the code is ./.")
+console.log("Sincerely, yours truly --Jideeh")
 window.onscroll = showNav;
 
 function showNav() {
